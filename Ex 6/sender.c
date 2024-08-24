f0ad7788d999a8bfe244205f8a5571f9ab7399b1
ShiivRS331 <220701331@rajalakshmi.edu.in>
2024-08-24 14:13:50 +0530

    Update sender.c

--- Ex 6/sender.c
+++ Ex 6/sender.c
@@ -1,6 +1,7 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdbool.h>
-#include<string.h>
+#include <string.h>
+
 #define MAX_SEQ 7
 #define BUFFER ((MAX_SEQ + 1) / 2)
 
@@ -9,27 +10,47 @@ struct frame {
     int ack_no;
     char info;
 };
+
 struct frame s;
 
-void send_frame(int frame_no, int frame_expected, char buff[]) {
-    //collect the data and print it 
+void send_frame(int frame_no, int frame_expected, char buff[], int message_length) {
+    // Collect the data and print it
     s.seq_no = frame_no;
     s.ack_no = (frame_expected + MAX_SEQ) % (MAX_SEQ + 1);
     s.info = buff[frame_no % BUFFER];
-    printf("Sending Frame %d: seq_no: %d, ack_no: %d, info: %c\n",frame_no, s.seq_no, s.ack_no,s.info);
+
+    // Handle wrapping around for messages longer than buffer
+    if (frame_no >= message_length) {
+        printf("Reached end of message, stopping transmission.\n");
+        return;
+    }
+
+    printf("Sending Frame %d: seq_no: %d, ack_no: %d, info: %c\n", frame_no, s.seq_no, s.ack_no, s.info);
+
 }
+
 int main() {
     int next_frame_to_send = 0;
     int ack_expected = 0;
     char out[BUFFER];
-    //get the message 
+    int message_length;
+
+    // Get the message
     printf("Enter the message: ");
-    scanf("%s",out);
-    //for each char in message send it and print it using send_frame()
-    for (int i = 0; i < strlen(out); i++) {
-        send_frame(next_frame_to_send, ack_expected, out);
-        //increment to next frame using formula to wrap around 
+    scanf("%s", out);
+    message_length = strlen(out);
+
+    // Handle buffer size mismatch (optional)
+    if (message_length > BUFFER) {
+        printf("Warning: Message length (%d) exceeds buffer size (%d). "
+               "Some characters may be dropped.\n", message_length, BUFFER);
+    }
+
+    // Send each character in the message, wrapping around as needed
+    for (int i = 0; i < message_length; i++) {
+        send_frame(next_frame_to_send, ack_expected, out, message_length);
         next_frame_to_send = (next_frame_to_send + 1) % (MAX_SEQ + 1);
     }
+    printf("\nAll frames Sent...Transmission Completed!");
     return 0;
 }
